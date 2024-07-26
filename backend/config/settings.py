fbc185cdec6f77e2d55b97ace22f479a74c36fe2
Tomás Camero <80246365+TomasCB24@users.noreply.github.com>
2024-07-26 14:42:48 +0200

    Solved comma error

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -134,7 +134,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-  'http://localhost:3000'
+  'http://localhost:3000',
   'https://simpleportfolio-production.up.railway.app:8080'
 ]
 
